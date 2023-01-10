d38b67df5eee52e05941ac5e5d78cd3c254381de
nakamotokano <121929712+nakamotokano@users.noreply.github.com>
2023-01-10 04:35:27 +0000

    20220110-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,37 @@
 class BooksController < ApplicationController
-  def top
+  def new
+    @book = Book.new
+    @book = Book.all
+  end
+
+  def create
+   book = Book.new(book_parmars)
+   book.save
+   redirect_to book_path(book.id)
+   #if @book.save
+  end
+
+  def index
+  @book =Book.new
+  end
+
+  def show
+    @book =Book.find(params[:id])
+  end
+
+  def update
+   book = Book.find(parmars[:id])
+   book.update(book_params)
+   redirect_to book_path(book.id)
+
+ end
+
+  def edit
+    @book =Book.find(params[:id])
+    end
+
+    private
+  def book_parmars
+    params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/books'=>'books#top'
-  post '/books'=>'books#top'
+  resources :books
   get '/'=>'homes#top'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
